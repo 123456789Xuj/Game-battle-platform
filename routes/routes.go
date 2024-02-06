849f09a9751b6b97a3e871e05eec321c15476f90
123456789Xuj <2638700369@qq.com>
2024-02-06 16:52:33 +0800

    master

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,31 @@
 package routes
+
+import (
+	"awesomeProject/api"
+	"awesomeProject/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouts() error {
+	r := gin.Default()
+	r.GET("/ShowMenu", api.ShowMenu)
+	r.POST("/Collection", api.Collectin)
+	r.POST("/UserLanding", api.UserLanding)
+
+	_ = r.Group("/homepage", middleware.JWT())
+	{
+		r.GET("", api.Home)
+		r.POST("/add", api.AddFriends)
+		r.POST("/creation", api.CreationRoom)
+		r.POST("/join", api.JoinRoom)
+		r.GET("/ready", api.Ready)
+		r.POST("/GameStart", api.Start)
+
+	}
+	err := r.Run()
+	if err != nil {
+		return err
+	}
+
+	return err
+}
