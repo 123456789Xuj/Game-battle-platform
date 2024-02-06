8fab21a29747e7b2e3e481a3015a742b31239269
123456789Xuj <2638700369@qq.com>
2024-02-06 16:52:01 +0800

    master

--- models/ini.go
+++ models/ini.go
@@ -1,1 +1,26 @@
 package models
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func InitMySQL() error {
+	dns := "root:xjl666nbsg@tcp(127.0.0.1:3306)/landing?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	DB, err = gorm.Open(mysql.Open(dns), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	_ = DB.AutoMigrate(&NewUser{})
+	_ = DB.AutoMigrate(&GameRoom{})
+	_ = DB.AutoMigrate(&GameRoom{})
+	_ = DB.AutoMigrate(&Chess{})
+	_ = DB.AutoMigrate(&League{})
+	return nil
+
+}
--- models/models.go
+++ models/models.go
@@ -1,1 +1,27 @@
 package models
+
+type NewUser struct {
+	ID          int
+	Username    string `json:"Username"`
+	PhoneNumber int    `json:"phone_number"`
+	Password    string `json:"password"`
+}
+type GameFriend struct {
+	ID       int
+	Username string `json:"username"`
+}
+type GameRoom struct {
+	ID           int
+	Username     string `json:"username"`
+	MainUsername string `json:"mainUsername"`
+	Ready        bool   `json:"ready"`
+}
+type Chess struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+type League struct {
+	ID       int
+	UserName string
+	Integral int
+}
