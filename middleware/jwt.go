f54f9544888215f8fe68daee9fa84f9dbc2def46
123456789Xuj <2638700369@qq.com>
2024-02-06 16:51:08 +0800

    master

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,1 +1,45 @@
 package middleware
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"net/http"
+)
+
+func JWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 从请求头中获取令牌信息
+		tokenString := c.GetHeader("Authorization")
+		if tokenString == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Missing authorization header"})
+			c.Abort()
+			return
+		}
+
+		// 验证令牌
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte("secret_key"), nil
+		})
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		// 检查令牌是否有效
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			// 将用户名添加到上下文中，以便在后续处理程序中使用
+			c.Set("username", claims["username"].(string))
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
